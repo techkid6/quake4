da67676d468b1c25840b8d0e6de64b7a8fe9410d
Evan Markowitz <evan.markowitz99@gmail.com>
2019-01-28 23:15:23 -0500

    Add in health diminishing

--- game/weapon/WeaponMachinegun.cpp
+++ game/weapon/WeaponMachinegun.cpp
@@ -234,6 +234,7 @@ stateResult_t rvWeaponMachinegun::State_Fire ( const stateParms_t& parms ) {
 				nextAttackTime = gameLocal.time + (fireRate * owner->PowerUpModifier ( PMOD_FIRERATE ));
 				Attack ( false, 1, spread, 0, 1.0f );
 			}
+			owner->health -= 1; // Let's give the player some damage -Evan
 			PlayAnim ( ANIMCHANNEL_ALL, "fire", 0 );	
 			return SRESULT_STAGE ( STAGE_WAIT );
 	
